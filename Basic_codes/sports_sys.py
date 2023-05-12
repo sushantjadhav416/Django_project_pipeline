1d0da6b5ebe3787663d0ce1d1e2ee11bce54f93c
Sushant jadhav <jadhavsushant.vikas@hcl.com>
2023-05-12 18:39:38 +0530

    cm1

--- Basic_codes/sports_sys.py
+++ Basic_codes/sports_sys.py
@@ -1,33 +1,70 @@
 class Sport:
     def __init__(self):
-            pass
-    def CalculateAvgage(ages):
           pass
-    def retierdPlayer(id):
+    def CalculateAvgage(self,ages):
           pass
-    
+    def retierdPlayer(self,id):
+          pass
 class Cricket(Sport):
     playerid=[]
-    
     def  __init__(self):
-       self.playerid.append(1)
+       for i in range(11):
+         self.playerid.append(i)
        print("The new cricket team is formed:")
-    def CalculateAvgage(ages):
+
+    def CalculateAvgage(self,ages):
           sum=0
-          for i in ages:
+          for i in range(len(ages)):
                sum+=ages[i]
           avg = sum/len(ages)
-          print(f"The average age of the team is: {avg}")
+          print(f"The average age of the Cricket team is: {avg}")
     
     def retierdPlayer(self,id):
         if(self.playerid[id-1]!=-1):
               self.playerid[id-1]=-1
-              print(f"The palyer with id {id} is retierd!!!")
+              print(f"The palyer with id {id} is retierd from cricket team!!!")
+        else:
+              print("The palyer is retired already !!!!!")
+class Football(Sport):
+    playerid=[]
+    def  __init__(self):
+       for i in range(11):
+          self.playerid.append(i)
+       print("The new cricket team is formed:")
+
+    def CalculateAvgage(self,ages):
+          sum=0
+          for i in ages:
+               sum+=ages[i]
+          avg = sum/len(ages)
+          print(f"The average age of the football team is: {avg}")
+    
+    def retierdPlayer(self,id):
+        if(self.playerid[id-1]!=-1):
+              self.playerid[id-1]=-1
+              print(f"The palyer with id {id} is retierd form football team!!!")
         else:
               print("The palyer is retired already !!!!!")
 
-class Football(Sport):
+if __name__=="__main__":
+     c1 = Cricket()
+     f1 = Football()
+     ages = [23,34,43,21,23,32,32,23,43,12,21]
+     id  = int(input("Enter the id:"))
+     c1.CalculateAvgage(ages)
+     f1.retierdPlayer(id)
+
+
+
+
+
+
+
      
+
+
+
+
 
 
 
