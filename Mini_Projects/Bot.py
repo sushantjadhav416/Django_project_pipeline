f0999b102af9bb7dac394fd718ba5ef8a55e304b
Sushant jadhav <jadhavsushant.vikas@hcl.com>
2023-06-19 14:30:37 +0530

    Code Updated

--- Mini_Projects/Bot.py
+++ Mini_Projects/Bot.py
@@ -1,5 +1,7 @@
 import pyttsx3
 import datetime
+import speech_recognition as myvc
+import wikipedia
 
 engine = pyttsx3.init('sapi5') 
 voices = engine.getProperty('voices')
@@ -14,17 +16,60 @@ def wishme():
 	hour=int(datetime.datetime.now().hour)
 	if hour>=0 and hour<12:
 		print(hour)
-		speak("Good morning sir!")
+		speak("Good morning !")
 
 	elif hour>=12 and hour<=18:
 		print(hour)
-		speak("Good Afternoon sir!")
+		speak("Good Afternoon !")
 	
 	elif hour>=19:
 		print(hour)
-		speak("Good evening sir!")
+		speak("Good evening !")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = myvc.Recognizer()
+    with myvc.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+
 
 if __name__ == "__main__":
-  wishme()
-  speak("Hello handsome, i'm FRIDAY !,How can i help you!")
-	 
+    wishme()
+    speak("Hi handsome i am friday your personal assistent")
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+
+            
+
+
+
+
+    
+       
